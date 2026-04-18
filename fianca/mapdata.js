e078dcbb46f9ba53f52af1943d12968ef723ad26
marcosdiegot <68549782+marcosdiegot@users.noreply.github.com>
2026-04-18 14:58:20 -0300

    feat: add simplemaps mapdata brazil states

---
+++ fianca/mapdata.js
@@ -0,0 +1,381 @@
+var simplemaps_countrymap_mapdata={
+  main_settings: {
+    //General settings
+		width: "responsive", //or 'responsive'
+    background_color: "#FFFFFF",
+    background_transparent: "yes",
+    border_color: "#ffffff",
+    pop_ups: "detect",
+    
+		//State defaults
+		state_description: "State description",
+    state_color: "#88A4BC",
+    state_hover_color: "#3B729F",
+    state_url: "",
+    border_size: 1.5,
+    all_states_inactive: "no",
+    all_states_zoomable: "yes",
+    
+		//Location defaults
+		location_description: "Location description",
+    location_url: "",
+    location_color: "#FF0067",
+    location_opacity: 0.8,
+    location_hover_opacity: 1,
+    location_size: 25,
+    location_type: "square",
+    location_image_source: "frog.png",
+    location_border_color: "#FFFFFF",
+    location_border: 2,
+    location_hover_border: 2.5,
+    all_locations_inactive: "no",
+    all_locations_hidden: "no",
+    
+		//Label defaults
+		label_color: "#ffffff",
+    label_hover_color: "#ffffff",
+    label_size: 16,
+    label_font: "Arial",
+    label_display: "auto",
+    label_scale: "yes",
+    hide_labels: "no",
+    hide_eastern_labels: "no",
+   
+		//Zoom settings
+		zoom: "yes",
+    manual_zoom: "yes",
+    back_image: "no",
+    initial_back: "no",
+    initial_zoom: "-1",
+    initial_zoom_solo: "no",
+    region_opacity: 1,
+    region_hover_opacity: 0.6,
+    zoom_out_incrementally: "yes",
+    zoom_percentage: 0.99,
+    zoom_time: 0.5,
+    
+		//Popup settings
+		popup_color: "white",
+    popup_opacity: 0.9,
+    popup_shadow: 1,
+    popup_corners: 5,
+    popup_font: "12px/1.5 Verdana, Arial, Helvetica, sans-serif",
+    popup_nocss: "no",
+    
+		//Advanced settings
+		div: "map",
+    auto_load: "yes",
+    url_new_tab: "no",
+    images_directory: "default",
+    fade_time: 0.1,
+    link_text: "View Website"
+  },
+  state_specific: {
+    BRAC: {
+      name: "Acre",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRAL: {
+      name: "Alagoas",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRAM: {
+      name: "Amazonas",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRAP: {
+      name: "Amapá",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRBA: {
+      name: "Bahia",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRCE: {
+      name: "Ceará",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRDF: {
+      name: "Distrito Federal",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRES: {
+      name: "Espírito Santo",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRGO: {
+      name: "Goiás",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRMA: {
+      name: "Maranhão",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRMG: {
+      name: "Minas Gerais",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRMS: {
+      name: "Mato Grosso do Sul",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRMT: {
+      name: "Mato Grosso",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRPA: {
+      name: "Pará",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRPB: {
+      name: "Paraíba",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRPE: {
+      name: "Pernambuco",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRPI: {
+      name: "Piauí",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRPR: {
+      name: "Paraná",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRRJ: {
+      name: "Rio de Janeiro",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRRN: {
+      name: "Rio Grande do Norte",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRRO: {
+      name: "Rondônia",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRRR: {
+      name: "Roraima",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRRS: {
+      name: "Rio Grande do Sul",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRSC: {
+      name: "Santa Catarina",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRSE: {
+      name: "Sergipe",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRSP: {
+      name: "São Paulo",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    },
+    BRTO: {
+      name: "Tocantins",
+      description: "default",
+      color: "default",
+      hover_color: "default",
+      url: "default"
+    }
+  },
+  locations: {
+    "0": {
+      name: "Brasília",
+      lat: "-15.783333",
+      lng: "-47.916667"
+    }
+  },
+  labels: {
+    BRAC: {
+      name: "Acre",
+      parent_id: "BRAC"
+    },
+    BRAL: {
+      name: "Alagoas",
+      parent_id: "BRAL"
+    },
+    BRAM: {
+      name: "Amazonas",
+      parent_id: "BRAM"
+    },
+    BRAP: {
+      name: "Amapá",
+      parent_id: "BRAP"
+    },
+    BRBA: {
+      name: "Bahia",
+      parent_id: "BRBA"
+    },
+    BRCE: {
+      name: "Ceará",
+      parent_id: "BRCE"
+    },
+    BRDF: {
+      name: "Distrito Federal",
+      parent_id: "BRDF"
+    },
+    BRES: {
+      name: "Espírito Santo",
+      parent_id: "BRES"
+    },
+    BRGO: {
+      name: "Goiás",
+      parent_id: "BRGO"
+    },
+    BRMA: {
+      name: "Maranhão",
+      parent_id: "BRMA"
+    },
+    BRMG: {
+      name: "Minas Gerais",
+      parent_id: "BRMG"
+    },
+    BRMS: {
+      name: "Mato Grosso do Sul",
+      parent_id: "BRMS"
+    },
+    BRMT: {
+      name: "Mato Grosso",
+      parent_id: "BRMT"
+    },
+    BRPA: {
+      name: "Pará",
+      parent_id: "BRPA"
+    },
+    BRPB: {
+      name: "Paraíba",
+      parent_id: "BRPB"
+    },
+    BRPE: {
+      name: "Pernambuco",
+      parent_id: "BRPE"
+    },
+    BRPI: {
+      name: "Piauí",
+      parent_id: "BRPI"
+    },
+    BRPR: {
+      name: "Paraná",
+      parent_id: "BRPR"
+    },
+    BRRJ: {
+      name: "Rio de Janeiro",
+      parent_id: "BRRJ"
+    },
+    BRRN: {
+      name: "Rio Grande do Norte",
+      parent_id: "BRRN"
+    },
+    BRRO: {
+      name: "Rondônia",
+      parent_id: "BRRO"
+    },
+    BRRR: {
+      name: "Roraima",
+      parent_id: "BRRR"
+    },
+    BRRS: {
+      name: "Rio Grande do Sul",
+      parent_id: "BRRS"
+    },
+    BRSC: {
+      name: "Santa Catarina",
+      parent_id: "BRSC"
+    },
+    BRSE: {
+      name: "Sergipe",
+      parent_id: "BRSE"
+    },
+    BRSP: {
+      name: "São Paulo",
+      parent_id: "BRSP"
+    },
+    BRTO: {
+      name: "Tocantins",
+      parent_id: "BRTO"
+    }
+  }
+};
